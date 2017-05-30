281bb9ed3333e4f047bc82864dd80b2b6425785b
lunaangelbabe <beccastephens1992@gmail.com>
2017-05-30 12:58:17 +1200

    Started on a self host << not error free yet

---
+++ Version 1/BookStoreSelfHost/DTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Version_1
+{
+    public class clsAuthor
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+    }
+}
